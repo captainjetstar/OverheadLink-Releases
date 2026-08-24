7bb66507070b8dae31ade27a369b5b9d484c7353
captainjetstar <jkittelty@icloud.com>
2026-08-24 21:17:34 +1000

    Run profile migration and install remote panel

--- run_overheadlink.py
+++ run_overheadlink.py
@@ -4,9 +4,14 @@ import sys
 ROOT = Path(__file__).resolve().parent
 sys.path.insert(0, str(ROOT / "src"))
 
-from overheadlink.app import main
+from overheadlink.bootstrap import install_app_extensions, prepare_profile
+
+prepare_profile()
+
+from overheadlink import app as app_module
+
+install_app_extensions(app_module.OverheadLinkApp)
 
 
 if __name__ == "__main__":
-    main()
-
+    app_module.main()
