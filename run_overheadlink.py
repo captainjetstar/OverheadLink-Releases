7855a0ff91f55c89ce3187badb06d899ad576b79
captainjetstar <jkittelty@icloud.com>
2026-08-24 21:39:04 +1000

    Use enhanced v0.3.8 runtime entrypoint

--- run_overheadlink.py
+++ run_overheadlink.py
@@ -4,14 +4,8 @@ import sys
 ROOT = Path(__file__).resolve().parent
 sys.path.insert(0, str(ROOT / "src"))
 
-from overheadlink.bootstrap import install_app_extensions, prepare_profile
-
-prepare_profile()
-
-from overheadlink import app as app_module
-
-install_app_extensions(app_module.OverheadLinkApp)
+from overheadlink.runtime import main
 
 
 if __name__ == "__main__":
-    app_module.main()
+    main()
